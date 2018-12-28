583277f7241bc13ed6ea8414345555f3957277fd
ramon <ramonestii@hotmail.com>
2018-12-28 19:37:23 -0300

    update routes master

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+  root 'pages#home'
   get 'order/create'
   get 'order/destroy'
   get 'order/update'
   #get 'pages/home'
 
-=======
+
   get 'user/create'
   get 'user/delete'
   get 'user/update'
   get 'pages/home'
->>>>>>> user_controller
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
