fe72b0bba3689eebc3197e8b43c6ef8b81446540
ramon <ramonestii@hotmail.com>
2018-12-28 19:33:05 -0300

    create order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get 'order/create'
+  get 'order/destroy'
+  get 'order/update'
+  #get 'pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
