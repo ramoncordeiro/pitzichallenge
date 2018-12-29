770c97a83551070d21a40c4ea5c5ae4c8f5e00ef
ramon <ramonestii@hotmail.com>
2018-12-28 21:14:41 -0300

    update user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,17 @@
 class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def create
+    @user = User.new(allowed_params)
+    if @user.save
+      flash[:notice] = 'Successfully created user.'
+
+      redirect_to user_path
+    else
+      render :new
+    end
   end
 
   def delete
@@ -8,3 +20,12 @@ class UserController < ApplicationController
   def update
   end
 end
+
+
+  private
+
+  def allowed_params
+    params.require(:user).permit(:name, :cpf, :email)
+  end
+end
+
